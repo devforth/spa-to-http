4a1be95e44a7f021e343b42362d4a335138e8464
Vladyslav Protasenia <vprotasenia@devforth.io>
2024-06-24 11:47:48 +0300

    bump version

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 docker buildx create --use
-docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/spa-to-http:latest" --tag "devforth/spa-to-http:1.0.6" --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag "devforth/spa-to-http:latest" --tag "devforth/spa-to-http:1.0.7" --push .
